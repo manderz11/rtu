d2fd81dff3aca2009721ed90bfad120e30493ed5
manderz11 <56352368+manderz11@users.noreply.github.com>
2019-11-26 15:58:56 +0200

    added last lections files

--- sketch.js
+++ sketch.js
@@ -1,14 +1,14 @@
 let canvas;
+let img;
 
 function setup() {
-canvas = createCanvas(windowWidth,windowHeight);
+canvas = createCanvas(1536,722);
 canvas.style('z-index', '-1');
-canvas.position(0,0)
+canvas.position(0,0);
 
 }
 
 function draw() {
-  background(0,200,0);
-
+  background(255);
 
 }
